0113789347b3dcbe757ed2123d9895a44709fd8b
sahiljoshi07 <79568685+sahiljoshi07@users.noreply.github.com>
2024-02-07 17:28:34 +0000

    Till Conversion

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,27 @@
-console.log("Sahil Joshi")
+//console.log("Sahil Joshi")
+//console.log("Anuj")
+let string1 = "Sai";
+string2 = true;
+// console.log(string1);
+// console.log(string2);
+// console.log(typeof (string1));
+// console.log(typeof (string2));
+
+let five = "5";
+
+// console.log(five);
+// console.log(typeof(five));
+
+let numFive = Number(five);
+//console.log(typeof(numFive));
+
+//console.log(3%2);
+
+let x = 4;
+let y = x++;  // x=5,y=4
+
+let z = 4;
+let w = ++z;  // z=5,w=5
+
+console.log(x);
+console.log(y);
